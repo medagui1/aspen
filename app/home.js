516891a8004b6edc6a41aa5a740b328d9c1ca20d
Med Agui <simohamedregragui2@gmail.com>
2024-01-19 11:54:09 +0100

    adding the tags list

--- app/home.js
+++ app/home.js
@@ -1,11 +1,35 @@
-import { Image, SafeAreaView, Text, TextInput, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import HomeHeader from "../assets/components/HomeHeader";
 
 import { location, arrow, search } from "../assets/icons";
 import { useState } from "react";
 
+const tags = [
+  "location",
+  "hotel",
+  "food",
+  "adventure",
+  "travel",
+  "exploration",
+  "destination",
+  "cuisine",
+  "excursion",
+];
+
+const capitalizeFirstLetter = (str) =>
+  `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [tagSelected, setTagSelected] = useState(tags[0]);
 
   return (
     <SafeAreaView className="p-6 pt-12">
@@ -34,6 +58,36 @@ const Home = () => {
       </View>
 
       {/* Tags */}
+      <View className='my-8'>
+        <FlatList
+          data={tags}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              className={`  p-4 rounded-full 
+            ${
+              tagSelected === item
+                ? "bg-[#176ff220] text-[#176ff2]"
+                : "text-[#b8b8b8]"
+            }`}
+              onPress={() => setTagSelected(item)}
+            >
+              <Text
+                className={`
+            ${
+              tagSelected === item
+                ? "text-[#176ff2] font-bold"
+                : "text-[#b8b8b8]"
+            }`}
+              >
+                {capitalizeFirstLetter(item)}
+              </Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
     </SafeAreaView>
   );
 };
